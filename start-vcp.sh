10b99e8c4e068d39b113ceb229c63eb404b5f184
kiwenlau <kiwenlau@126.com>
2015-12-24 16:17:07 +0900

    commit test dataset within galaxy image

--- start-vcp.sh
+++ start-vcp.sh
@@ -10,8 +10,12 @@ docker run -d --name mesosmaster --net=host -e MY_HOST_IF=eth1 -v /opt:/opt nasu
 # start mesosslave container
 docker run -d --name mesosslave --privileged -v /var/lib/docker:/var/lib/docker -v /opt:/opt --net=host -e MY_HOST_IF=eth1 -e ZK_HOST=127.0.0.1 -e MESOS_CPUS=8 -e MESOS_MEM=16000 -e MESOS_DISK=4000 kiwenlau/mesos-aurora:0.1
 
+
+rm /opt/workdir/galaxy_files/000/*
+cp ./galaxy/dataset/* /opt/workdir/galaxy_files/000/ 
+
 # start galaxy container
-docker run -d --name galaxy -v /opt:/opt --net=host kiwenlau/pitagora-galaxy:0.1
+docker run -d --name galaxy -v /opt:/opt --net=host kiwenlau/pitagora-galaxy:0.2
 
 echo -e "\nGalaxy is starting, please wait for a few seconds"
 
